c20f50a4254e1a0d44fde769871d8b2eb3a057aa
AnuragM042 <anurazzz.mishra098@gmail.com>
2024-11-06 15:23:59 +0530

    Automated commit from Node.js

--- src/services/fileService.js
+++ src/services/fileService.js
@@ -1,16 +1,16 @@
 // src/services/fileService.js
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // Define the file path to be created or updated
-const filePath = path.join(__dirname, '..', '..', 'example.txt');
+const filePath = path.join(__dirname, "..", "..", "example.txt");
 
 // Function to create or update the file
 function createOrUpdateFile() {
   return new Promise((resolve, reject) => {
-    fs.writeFile(filePath, 'Automated commit by Node.js\n', (err) => {
+    fs.writeFile(filePath, "Automated commit by Node.js\n", (err) => {
       if (err) reject(`Error creating/updating file: ${err}`);
-      else resolve('File created/updated successful ly');
+      else resolve("File created/updated successful ly");
     });
   });
 }
